13384ea9d7569fe13d2b310b02d417539192fcb4
Sol Vitkin <svitkin@Sols-Air.home>
2021-03-08 08:51:16 -0500

    Convert raw points and service json to dataframes

--- R/tosdr-data-pull.R
+++ R/tosdr-data-pull.R
@@ -27,3 +27,27 @@ load_json <- function(URL, output_filename) {
 
 services_json <- load_json('https://edit.tosdr.org/api/v1/services', "services.json")
 points_json <- load_json('https://edit.tosdr.org/api/v1/points', "points.json")
+
+# Create dataframes from original json data
+sample_points_df <- 
+  map_df(points_json[[1]][1:100], data.frame)
+sample_services_df <- map_df(services_json[[1]][1:100], data.frame)
+
+# Clean points data
+lower_case_text <- function(df) {
+  df %>% 
+    mutate(quoteText_clean = str_to_lower(quoteText))
+}
+clean_html_markup <- function(df) {
+  df %>% 
+    mutate(quoteText_clean = gsub("<.*?>", "", quoteText_clean))
+}
+clean_quote_text <- function(df) {
+  df %>% 
+    lower_case_text() %>% 
+    clean_html_markup()
+}
+# TODO: Remove stop words
+# TODO: Make a filter for "bad" quoteText
+# TODO: Limit number of labels in the title column to a few of the more frequent ones
+sample_points_df_clean <- clean_quote_text(sample_points_df)
